ffb246c1ca3b934f882ee886863cfcbced03596d
Shourov Foisal <shourovfoisal@gmail.com>
2023-04-28 21:38:00 +0600

    Added homepage and rendered data in react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,29 @@
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Homepage from "./pages/Homepage";
+import ReviewDetails from "./pages/ReviewDetails";
+import Category from "./pages/Category";
+import SiteHeader from "./components/SiteHeader";
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <SiteHeader />
+        <Routes>
+          <Route exact path="/" element={<Homepage />}>
+          </Route>
+          
+          <Route path="/details/:id" element={<ReviewDetails />}>
+          </Route>
+
+          <Route path="/category/:id" element={<Category />}>
+          </Route>
+          
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
