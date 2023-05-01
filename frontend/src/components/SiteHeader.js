8b9cb670e4e0c62c69287c20fe395c3babf8e1c2
Shourov Foisal <shourovfoisal@gmail.com>
2023-05-01 11:26:35 +0600

    Displayed category list in the top navigation
    Added category schema to strapi and made many to many relation with review. In the SiteHeader component, fetched the categories using GraphQL and displayed them using Router Link.

--- frontend/src/components/SiteHeader.js
+++ frontend/src/components/SiteHeader.js
@@ -1,10 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { gql, useQuery } from "@apollo/client";
+
+const CATEGORIES = gql`
+  query GetCategories {
+    categories {
+      data {
+          id
+          attributes {
+            name
+          }
+      }
+    }
+  }
+`;
 
 export default function SiteHeader() {
+
+  const {loading, data, error} = useQuery(CATEGORIES);
+  console.log(data);
+
   return (
     <div className='site-header'>
         <Link to="/"><h1>Ninja Reviews</h1></Link>
+        <nav className="categories">
+          <span>Filter reviews by category:</span>
+          {
+            data.categories.data.map(category => 
+              <Link key={category.id} to={`/category/${category.id}`}>{category.attributes.name}</Link>
+            )
+          }
+        </nav>
     </div>
   )
 }
